a0880f7341ca76641a79431270f1abdeeb2aec7c
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-07-12 03:19:41 +0300

    Update

---
+++ SF_Module16_Practice.Tests/CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+
+namespace SF_Module16_Practice.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Additional(5, 10) == 15);
+        }
+        
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Subtraction(15, 5) == 10);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Miltiplication(10, 5) == 50);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Division(15, 5) == 3);
+        }
+
+        [Test]
+        public void DivisionMustThrowDivideByZeroException()
+        {
+            var calculator = new Calculator();
+
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(5, 0));
+        }
+    }
+}
---
+++ SF_Module16_Practice/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace SF_Module16_Practice
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
